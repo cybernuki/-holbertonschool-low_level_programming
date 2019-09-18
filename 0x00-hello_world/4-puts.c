c48edde401a7b563669f0af5a0605e341fa87e2d
jhonatan arenas <1164@holbertonschool.com>
2019-09-18 16:34:36 +0000

    Challenge 4: fixing betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-  char string[100];
-  
-  strcpy(string, "\"Programming is like building a multilingual puzzle");
-
-  puts(string);
-
-  return (0);
+int main(void)
+{
+	char string[100];
+	strcpy(string, "\"Programming is like building a multilingual puzzle");
+	puts(string);
+	return (0);
 }

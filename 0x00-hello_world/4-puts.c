89065390ada7e0f12e0d627e7445ca2468ad6b5e
jhonatan arenas <1164@holbertonschool.com>
2019-09-18 18:47:02 +0000

    Fixing warning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-
 int main(void)
 {
-	char string[100];
-	strcpy(string, "\"Programming is like building a multilingual puzzle");
-	puts(string);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

75d065762b53ba6d2fb63ca05151b1630b011506
jhonatan arenas <1164@holbertonschool.com>
2019-09-19 17:28:50 +0000

    Fixing betty style

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,19 @@
 /*Headers*/
 
 /*main function*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char letter;
-	
-	for (letter = 'a'; letter<='z'; letter++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }

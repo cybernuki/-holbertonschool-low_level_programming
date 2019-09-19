adf26678fdb14befca1500b9ff952e66d614dd2e
jhonatan arenas <1164@holbertonschool.com>
2019-09-19 17:31:58 +0000

    Adding main information

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,13 +2,19 @@
 /*Header*/
 
 /*Main function*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
 	for (i = 'A'; i <= 'Z'; i++)
 		putchar(i);
 	putchar('\n');
-	return(0);
+	return (0);
 }

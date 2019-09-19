5852fd035ce0e1c8ab00f5d92330e99b76131723
jhonatan arenas <1164@holbertonschool.com>
2019-09-19 17:36:24 +0000

    Fixing betty style

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,12 +2,16 @@
 /*Header*/
 
 /*Main function*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
-	
-	for(i=0; i < 100; i += 10)
+	for(i = 0; i < 100; i += 10)
 		printf("%d", (i/10));
 	printf("\n");
-	return(0);
+	return (0);
 }

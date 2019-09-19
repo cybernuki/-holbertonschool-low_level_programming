ff5086f61c92b5bce4bdda9cf5f2427ebe467fa6
jhonatan arenas <1164@holbertonschool.com>
2019-09-19 17:35:13 +0000

    Adding coment of the function

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,14 +2,20 @@
 /*Header*/
 
 /*Main function*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		if(i != 'q' && i != 'e')
 			putchar(i);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }

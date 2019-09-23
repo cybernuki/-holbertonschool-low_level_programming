467a24e24f5701aca59b9500266b5c4f8f64c417
jhonatan arenas <1164@holbertonschool.com>
2019-09-23 15:03:49 +0000

    Challenge 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,59 @@
-0-holberton.c
+#include <stdio.h>
+#include <ctype.h>
+
+int print_sign(int n);
+
+/**
+ * main - Entry point
+ * Des: main function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = print_sign(98);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(0);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(0xff);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	r = print_sign(-1);
+	putchar(',');
+	putchar(' ');
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
+	return (0);
+}
+
+int print_sign(int c)
+{
+	int result;
+
+	if (c > 0)
+	{
+		result = 1;
+		putchar('+');
+	}
+	else if (c < 0)
+	{
+		result = -1;
+		putchar('-');
+	}
+	else
+	{
+		result = 0;
+		putchar('0');
+	}
+	return (result);
+}

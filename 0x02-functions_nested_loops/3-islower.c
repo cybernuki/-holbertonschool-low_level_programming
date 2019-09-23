51f1bcc3d139e456aafcc787ffdcc5b152648477
jhonatan arenas <1164@holbertonschool.com>
2019-09-23 13:54:27 +0000

    Challenge 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,31 @@
-0-holberton.c
+#include <stdio.h>
+#include <ctype.h>
+
+int _islower(int c);
+
+/**
+ * main - Entry point
+ * Des: main function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	putchar(r + '0');
+	r = _islower('o');
+	putchar(r + '0');
+	r = _islower(108);
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
+}
+
+int _islower(int c)
+{
+	int result;
+
+	result = (islower(c) > 0 ? 1 : 0);
+	return (result);
+}

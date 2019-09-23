2c745b239b379ddc63a137749ae229827d4cf50d
jhonatan arenas <1164@holbertonschool.com>
2019-09-23 13:32:55 +0000

    Challenge 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,25 @@
-0-holberton.c
+#include <stdio.h>
+
+
+void print_alphabet(void);
+/**
+ * main - Entry point
+ * Des: main function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}

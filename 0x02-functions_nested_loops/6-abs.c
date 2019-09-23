5349b79ba5dc622b0e973c42c30ae26045f2cf98
jhonatan arenas <1164@holbertonschool.com>
2019-09-23 15:17:55 +0000

    Challenge 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,30 @@
-0-holberton.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int _abs(int);
+
+
+/**
+ * main - Entry point
+ * Des: main function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
+
+int _abs(int c)
+{
+	return (abs(c));
+}

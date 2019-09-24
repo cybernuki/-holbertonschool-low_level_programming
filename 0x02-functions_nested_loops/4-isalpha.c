3e6b3cf79e9c19fa9381a22b9d9c1badf090d478
jhonatan arenas <1164@holbertonschool.com>
2019-09-24 13:42:34 +0000

    fixing logic

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,6 +9,7 @@ int _isalpha(int c)
 {
 	int result;
 
-	result = ((_islower(c) == 1) || ((c >= 'A') && (c <= 'Z'))) ? 1 : 0;
+	result = (((c >= 'a') && (c <= 'z'))
+		  || ((c >= 'A') && (c <= 'Z'))) ? 1 : 0;
 	return (result);
 }

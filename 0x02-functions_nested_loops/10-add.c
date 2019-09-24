4d4728e2b5fa25fe5b7e0387d470cd03b08dc0e9
Punpun <1164@holbertonschool.com>
2019-09-24 02:38:08 +0000

    Challenge 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,12 @@
-0-holberton.c
+/**
+ * add - add two integers and returns the result
+ * @c: First number
+ * @d: Second number
+ *
+ * Return: the result of adding c to d
+ */
+
+int add(int c, int d)
+{
+	return (c + d);
+}

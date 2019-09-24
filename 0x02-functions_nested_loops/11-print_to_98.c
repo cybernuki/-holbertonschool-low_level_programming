67010738635a3b231aadd3ceb4f593228094dd62
Punpun <1164@holbertonschool.com>
2019-09-24 02:53:31 +0000

    Challenge 11 and holberton.h updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,20 @@
-0-holberton.c
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all natural numbers from n to 98,
+ * followed by a new line.
+ * @n: given number
+ *
+ * Return: does not have return
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+
+                /*update*/
+		n = (n < 98) ? (n + 1) : (n - 1);
+	}
+	printf("98\n");
+}

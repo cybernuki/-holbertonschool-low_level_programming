170109ead2dd7218997b3f4a2c54aee27bf46447
Punpun <1164@holbertonschool.com>
2019-09-24 02:32:15 +0000

    Challenge 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,35 @@
-0-holberton.c
+#include "holberton.h"
+
+/**
+ * times_table - Prints 9 times table,
+ * starting with 0
+ * @void: No parameter
+ *
+ * Return: no return
+ */
+
+void times_table(void)
+{
+	int i, j, factor;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		for (j = 1; j < 10; j++)
+		{
+			factor = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if ((factor / 10) != 0)
+			{
+				_putchar((factor / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((factor % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

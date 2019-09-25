440f20f8394274cf57f863920ce1c6c1976390bd
Punpun <1164@holbertonschool.com>
2019-09-25 20:57:09 +0000

    even fibonacci numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,31 @@
-0-holberton.c
+#include <stdio.h>
+/**
+ * main - prints all even-value fibonacci numbers less than 4.000.000
+ * @void: It doesn't recive any parameter
+ * Return: 0 - 1 or -1
+ */
+int main(void)
+{
+	unsigned long int f_1, f_2, fibonacci_number;
+
+	f_1 = 0;
+	f_2 = 1;
+	fibonacci_number = 0;
+
+	while (fibonacci_number < 4000000)
+	{
+		fibonacci_number = f_1 + f_2;
+
+		if (fibonacci_number % 2 == 0)
+		{
+			printf("%lu", fibonacci_number);
+
+			if ((f_2 + fibonacci_number) < 4000000)
+				printf(", ");
+		}
+		f_1 = f_2;
+		f_2 = fibonacci_number;
+	}
+	printf("\n");
+	return (0);
+}

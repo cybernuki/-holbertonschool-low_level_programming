aa4996eda96c7aff3948054c8e66673997424dc1
Punpun <1164@holbertonschool.com>
2019-09-25 22:37:18 +0000

    98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,25 @@
-0-holberton.c
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - prints the first 98 fibonacci numbers
+ * @void: It doesn't recive any parameter
+ * Return: 0 - 1 or -1
+ */
+int main(void)
+{
+	double phi;
+	int i;
+
+	phi = (1 - sqrt(5)) / 2;
+
+	for (i = 1; i <= 99; i++)
+	{
+		printf("%1.0f",
+		       ((pow(phi, i) - pow((phi * -1), (i * -1))) / sqrt(5))
+		       * -1);
+		if (i < 99)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}

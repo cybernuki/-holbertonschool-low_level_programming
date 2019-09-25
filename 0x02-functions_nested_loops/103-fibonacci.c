e19362206e1260b64e1a1bf12024d3ebc07707fd
Punpun <1164@holbertonschool.com>
2019-09-25 21:18:33 +0000

    adding the sum of the even numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,23 +6,23 @@
  */
 int main(void)
 {
-	long int f_1, f_2, fibonacci_number;
+	long int f_1, f_2, fibonacci_number, result;
 
 	f_1 = 0;
 	f_2 = 1;
 	fibonacci_number = 0;
+	result = 0;
 
 	while (fibonacci_number < 4000000)
 	{
 		fibonacci_number = f_1 + f_2;
 
-		if (fibonacci_number % 2 == 0)
-		{
-			printf("%ld", fibonacci_number);
-		}
+		result = (fibonacci_number % 2 == 0) ?
+			result + fibonacci_number : result;
+
 		f_1 = f_2;
 		f_2 = fibonacci_number;
 	}
-	printf("\n");
+	printf("%li\n", result);
 	return (0);
 }

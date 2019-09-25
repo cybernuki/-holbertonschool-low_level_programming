2f83154ddf66f89552f3ab3df915ab98ff00ba0a
Punpun <1164@holbertonschool.com>
2019-09-25 16:40:36 +0000

    Advance 1

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,26 @@
-0-holberton.c
+#include <stdio.h>
+
+/**
+ * main - Print the sum of all numbers that are
+ * divisible by 3 and 5
+ *
+ * @void: doesn't recive parameters
+ *
+ * Return: 0, 1 or -1
+ */
+int main(void)
+{
+	int i, result;
+
+	result = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			result += i;
+		}
+	}
+	printf("%d\n", result);
+
+	return (0);
+}

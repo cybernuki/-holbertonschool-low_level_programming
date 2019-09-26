7665ae8a590c55186b280469b806bae7183fcfb1
jhonatan arenas <1164@holbertonschool.com>
2019-09-26 21:35:09 +0000

    Fixing

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,11 +10,23 @@ void print_number(int n)
 	int i, power;
 
 	power = 1;
+	while ((n / power) > 10)
+	{
+		power *= 10;
+	}
 
 	if (n < 0)
+		_putchar('-');
+
+	if (n == 0)
+		_putchar('0');
+
+	else
 	{
-		
+		for (i = power; i >= 1 ; i /= 10)
+		{
+			_putchar(((n / i) % 10) + '0');
+		}
 	}
 	_putchar('\n');
-
 }

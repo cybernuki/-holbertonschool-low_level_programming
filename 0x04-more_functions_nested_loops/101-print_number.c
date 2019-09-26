ffca092fd87aa5eed9d13532969b1673ce7ef137
jhonatan arenas <1164@holbertonschool.com>
2019-09-26 21:26:37 +0000

    Adding last exercise

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: an intenger
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i, power;
+
+	power = 1;
+
+	if (n < 0)
+	{
+		
+	}
+	_putchar('\n');
+
+}

acf77a73f0684f305970d9210fa0524791112b81
jhonatan arenas <1164@holbertonschool.com>
2019-09-26 14:43:59 +0000

    Challenge 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - Prints the numbers, from 0 to 9,
+ * followed by a new line. Also, don't print 2 and 4
+ * @void: no parameter
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int  i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}

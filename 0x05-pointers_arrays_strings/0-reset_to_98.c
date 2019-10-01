0cc14c0c1a698acdc37c8fc069cc310b4ef17583
jhonatan arenas <1164@holbertonschool.com>
2019-10-01 13:57:45 +0000

    Challenge 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter and updates
+ * the value it points to 98.
+ *
+ * @n: is a pointer to an int
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+# Pointers, Arrays and strings
+This project contains some exercises about using pointers, arrays and strings
+in c.
+
+## Exercises:
+0-reset_to_98.c

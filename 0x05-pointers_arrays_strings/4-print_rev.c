638d7c15719b92dca9e59ffff3c7277022771b6f
jhonatan arenas <1164@holbertonschool.com>
2019-10-01 15:29:42 +0000

    Fixing ending point

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,15 @@
  */
 void print_rev(char *s)
 {
-	char *start = s - 1;
+	char *start = s;
 
 	while (*s != '\0')
 		s++;
 
 	while (s != start)
 	{
-		_putchar(*s);
 		s--;
+		_putchar(*s);
 	}
 	_putchar('\n');
 }

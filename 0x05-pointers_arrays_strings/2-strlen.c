187e586634e1e764e93a27bd87d899129273c000
jhonatan arenas <1164@holbertonschool.com>
2019-10-01 14:28:21 +0000

    Challenge 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen(char *s) - Returns the length of a string
+ * @s: is a pointer to the first value of a string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != 0)
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}

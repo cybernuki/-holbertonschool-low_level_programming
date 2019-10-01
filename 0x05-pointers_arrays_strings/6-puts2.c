deee54d3b22add84a56fdde30cfd93e7aa0d21d1
jhonatan arenas <1164@holbertonschool.com>
2019-10-01 19:13:18 +0000

    Challenge 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: a string of characters
+ * Returns : void
+ */
+
+void puts2(char *str)
+{
+	int  i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+}

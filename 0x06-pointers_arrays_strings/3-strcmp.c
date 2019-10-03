d0f9ea93bc9883fe303850765b5e7bb901a71fe9
jhonatan arenas <1164@holbertonschool.com>
2019-10-03 17:44:17 +0000

    Challenge 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if are the same in size,a number greater than 0 if
+ * it is greater or a number less that 0 in otherwise.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result = 0;
+	int size_s1 = 0;
+	int size_s2 = 0;
+
+	while (*(s1 + size_s1) != '\0')
+		size_s1++;
+
+	while (*(s2 + size_s2) != '\0')
+		size_s2++;
+
+	result = (size_s1 < size_s2) ? -5 : 5;
+
+	return (result);
+}

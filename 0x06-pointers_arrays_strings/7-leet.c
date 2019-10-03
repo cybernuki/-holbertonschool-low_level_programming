b62795b66172ec0a14e4426f5d1957384d076ba4
jhonatan arenas <1164@holbertonschool.com>
2019-10-03 22:11:38 +0000

    Challenge 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+/**
+ * leet - encodes a string into 1337
+ * @s: a string
+ * Return: a pointer to the string
+ */
+char *leet (char *s)
+{
+	char *s_copy = s;
+	char leet[] = {'4', '3', '0', '7', '1'};
+	char lower[] = {'a', 'e', 'o', 't', 'l'};
+	char upper[] = {'A', 'E', 'O', 'T', 'L'};
+	int index;
+
+	while (*s_copy != '\0')
+	{
+		for (index = 0; index < 4; index++)
+		{
+			if (*s_copy == lower[index] || *s_copy == upper[index])
+			{
+				*s_copy = leet[index];
+				break;
+			}
+		}
+		s_copy++;
+	}
+	return (s);
+}

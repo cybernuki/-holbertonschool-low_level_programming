da70d7830b989cb38cc8a748a8f93e3478173e73
jhonatan arenas <1164@holbertonschool.com>
2019-10-07 18:46:09 +0000

    Challenge 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+/**
+ * _strchr - Locates a character in a string
+ * @s: a string;
+ * @c: character to be searched
+ * Return: a pointer to to first ocurrence or the same pointer with the null
+ *         reference.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *p = '\0';
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			break;
+		}
+		i++;
+	}
+
+	return (p);
+}

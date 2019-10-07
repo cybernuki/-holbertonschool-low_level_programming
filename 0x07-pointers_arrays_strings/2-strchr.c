8e6c0eb1c44cff001ee287ebc81a39cb3e4516e3
jhonatan arenas <1164@holbertonschool.com>
2019-10-07 19:47:51 +0000

    Another logic

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,22 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	if (c == '\0')
+	while (1)
 	{
-		return (0);
+		if (*s == c)
+			return (s);
+		if (*s == '\0')
+			return (0);
+		s++;
 	}
-
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			return (s + i);
-		}
-		i++;
-	}
-
 
 	return (0);
 }

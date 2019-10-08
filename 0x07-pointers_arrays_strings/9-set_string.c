a5bacaf2c73e351e50b213e491271b877173c65e
jhonatan arenas <1164@holbertonschool.com>
2019-10-08 01:05:19 +0000

    Challenge 9

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,3 +1,9 @@
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: the pointer that will points another pointer
+ * @to: the pointer to be pointed
+ * Return: Nothing
+ */
 void set_string(char **s, char *to)
 {
 	*s = to;

da64f83899e3662541c8d8a2029f774024d7e426
jhonatan arenas <1164@holbertonschool.com>
2019-10-09 20:29:44 +0000

    Betty style

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -39,8 +39,8 @@ int palindrome(char *s, int first, int last)
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        else
-                return (_strlen_recursion(s + 1) + 1);
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
 }

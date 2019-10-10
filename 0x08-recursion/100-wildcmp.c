ab9120123c9156362a81ba12ea06ff0c1fb9cd7c
jhonatan arenas <1164@holbertonschool.com>
2019-10-10 21:22:33 +0000

    Advance 0

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,23 @@
+/**
+ * wildcmp - Compares two integers considered the wildcards
+ * @s1: string to be compared
+ * @s2: String to compare
+ * Return: 1 if the strings can be considered identical, otherwise return
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		else if (wildcmp(s1, s2 + 1))
+			return (1);
+		else
+			return (wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}

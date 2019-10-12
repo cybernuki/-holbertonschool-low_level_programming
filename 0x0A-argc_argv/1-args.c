fc376426b67077b5af0162f69013f692d2321f67
Punpun <1164@holbertonschool.com>
2019-10-12 17:37:02 +0000

    Challenge 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * @argc: an integer with the number of parameters of the function
+ * @argv: a pointer to the strings.
+ * Return: 0 if the programm finished without problems, 1 in otherwise
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}

0d78aecf4505bd64c880eac6dc8a38577f5a1008
Punpun <1164@holbertonschool.com>
2019-10-17 23:33:16 +0000

    Fixing memory leak

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,8 +34,13 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
+	}
+	for (i = 0; i < height; i++)
+	{
 		for (j = 0; j < height; j++)
+		{
 			grid[i][j] = 0;
+		}
 	}
 	return (grid);
 }

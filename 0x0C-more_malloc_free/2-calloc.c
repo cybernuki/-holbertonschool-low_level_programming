ca7ab62a73a8ee0929613bdae58c00decb2e58fc
Punpun <1164@holbertonschool.com>
2019-10-17 17:34:35 +0000

    Fixing challenge 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
+	unsigned int i;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
@@ -18,6 +19,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
+	i = 0;
 
-	return (memset(p, 0, nmemb));
+	for (i = 0; i < nmemb * size; i += size)
+		p[i] = 0;
+
+	return (p);
 }

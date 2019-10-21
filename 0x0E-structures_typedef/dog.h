a11fcbe77fe05107d0dc6c009285824a0d3289bd
jhonatan arenas <1164@holbertonschool.com>
2019-10-21 23:52:24 +0000

    Dog!

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,5 @@ typedef struct dog
 #include <stdio.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif

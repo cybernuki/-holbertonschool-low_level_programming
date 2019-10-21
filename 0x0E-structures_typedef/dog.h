dd5c9ace8e58015b59d41734724c32f99d2132e9
jhonatan arenas <1164@holbertonschool.com>
2019-10-21 20:53:23 +0000

    Challenge 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG
+#define _DOG
+/**
+ * struct dog - is a struct of a dog
+ * @name: Name of the dog
+ * @age: Age of the dog.
+ * @owner: Owner of the dog
+ * Description: representate a data for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

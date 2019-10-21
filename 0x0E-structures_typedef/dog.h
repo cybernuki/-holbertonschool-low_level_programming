8774bf880eaaf71d81fa85bc7fc20434d5ac35c8
jhonatan arenas <1164@holbertonschool.com>
2019-10-21 22:12:14 +0000

    Challenge 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: Owner of the dog
  * Description: representate a data for a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 #include <stdlib.h>
 #include <stdio.h>

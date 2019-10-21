a98dece7a7ba3edde4da3d64b7c426c2520aec0c
jhonatan arenas <1164@holbertonschool.com>
2019-10-21 23:56:57 +0000

    using custom funcitons

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
-#include <stdlib.h>
 #include "dog.h"
-#include <string.h>
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _strlen(char *s);
+
 /**
  * new_dog - Creates a new dog
  * @name: is the name of the dog
@@ -16,8 +17,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL && owner == NULL)
 		return (NULL);
 
-	name_len = strlen(name);
-	owner_len = strlen(owner);
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
@@ -31,9 +32,40 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog->owner == NULL)
 		return (NULL);
 
-	memcpy(dog->name, name, name_len);
-	memcpy(dog->owner, owner, owner_len);
+	_memcpy(dog->name, name, name_len);
+	_memcpy(dog->owner, owner, owner_len);
 	dog->age = age;
 
 	return (dog);
+}
+/**
+ * _strlen_recursion - returns the length of a string, recursively
+ * @s: a pointer to the first value of the string
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen(s + 1) + 1);
+}
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area destination
+ * @src: memory area source
+ * @n: number of bytes to copie
+ * Return: a pointer to the destination memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
 }

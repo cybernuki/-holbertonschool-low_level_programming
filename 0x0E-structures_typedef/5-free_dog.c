da01c82a2152612472163945bb0f68724c685c95
jhonatan arenas <1164@holbertonschool.com>
2019-10-22 00:14:06 +0000

    Challenge 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+/**
+ * free_dog - frees dogs
+ * @d: the dog struct
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+}

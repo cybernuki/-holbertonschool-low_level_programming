8a8cb2834d1b1235a16e46f6a48a04687254bb92
Jhonatan <danfeedev@gmail.com>
2019-10-23 16:53:43 -0500

    Challenge 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ * get_op_func - it selects the correct function to perform
+ * @s: is the string operation
+ * Return: a function to a operation
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+/**
+ * main - do a given operation of two numbers
+ * @argc: is the number of parameters
+ * @argv: are the parameters strings
+ * Return: the finish of the operation
+ */
+int main(int argc, char **argv)
+{
+	int a, b, result;
+	int (*function)(int, int);
+
+	if (argc != 4)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	function = get_op_func(argv[2]);
+
+	if (function == NULL)
+	{
+		printf("ERROR\n");
+		exit(99);
+	}
+
+	result = function(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}

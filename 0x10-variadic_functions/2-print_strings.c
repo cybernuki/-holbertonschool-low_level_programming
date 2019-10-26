7e008c12a240b183033c75f25716aa2c66869dc9
jhonatan arenas <1164@holbertonschool.com>
2019-10-26 08:07:36 +0000

    Challenge 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - it prints strings followed by a new line.
+ * @separator: is the separation char to each number
+ * @n: it is the number of parameters
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	if (separator != NULL)
+	{
+		if (n == 0)
+			printf("\n");
+		else
+		{
+			unsigned int i = n;
+			va_list ap;
+			char *str;
+
+			va_start(ap, n);
+
+			while (i > 0)
+			{
+				str = va_arg(ap, char*);
+
+				if (str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", str);
+
+				if (i > 1)
+					printf("%s", separator);
+				i--;
+			}
+			printf("\n");
+			va_end(ap);
+		}
+	}
+}

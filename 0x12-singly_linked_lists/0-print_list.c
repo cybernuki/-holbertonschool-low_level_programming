cec66628624d80af7b08eeb312dae75c6662b671
jhonatan arenas <1164@holbertonschool.com>
2019-10-31 18:15:46 +0000

    Challenge 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: is the node head.
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int numNodes = 0;
+	const list_t *node = h;
+
+	while (node != NULL)
+	{
+		numNodes++;
+		if (node->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", node->len, node->str);
+		}
+		node = node->next;
+	}
+	return (numNodes);
+}

6d120768304f46804b74149caee5d7cd254be53f
jhonatan arenas <1164@holbertonschool.com>
2019-10-31 22:03:33 +0000

    Challenge 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * add_node_end - creates a new node at the end of the linked list
+ * @head: is the reference to the head;
+ * @str: is the string to be saved
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node = NULL, *actualNode;
+
+	actualNode = *head;
+	if (actualNode == NULL)
+		node = add_node(&actualNode, str);
+
+	while (actualNode != NULL)
+		actualNode = actualNode->next;
+
+	node = add_node(&actualNode, str);
+	return (node);
+}
+/**
+ * add_node - creates a new node at the end of the linked list
+ * @head: is the reference to the head;
+ * @str: is the string to be saved
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+	int size = 0;
+
+	node = malloc(sizeof(node));
+
+	if (node == NULL)
+		return (NULL);
+
+	while (str[size] != '\0')
+		size++;
+
+	node->str = strdup(str);
+	node->len = size;
+	node->next = *head;
+	*head = node;
+	return (node);
+}

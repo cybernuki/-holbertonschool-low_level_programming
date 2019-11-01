9bcec689dab7a961ba6bfdb26042bab176bcc76a
Cybernuki <1164@holbertonschool.com>
2019-10-31 21:16:24 -0500

    Advance 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void im_the_hire(void) __attribute__ ((constructor));
+
+
+/**
+ * im_the_hire - prints something before the main.
+ * Return: Nothing
+ */
+void im_the_hire(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

6ea43727acf69bf071ee3310de18f0013e1a5c05
Cybernuki <1164@holbertonschool.com>
2019-11-05 11:06:35 -0500

    Challenge 100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *curr = NULL, *next = NULL;
+
+	if (!head || !*head)
+		return (NULL);
+
+	curr = *head;
+	*head = NULL;
+	while (curr)
+	{
+		next = curr->next;
+		curr->next = *head;
+		*head = curr;
+		curr = next;
+	}
+
+	return (*head);
+}

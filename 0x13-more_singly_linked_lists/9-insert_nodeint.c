6870e4890589c8887e1b1b02011ea2d849bf01f1
Cybernuki <1164@holbertonschool.com>
2019-11-05 14:48:58 -0500

    Controlling segmentation fault

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,6 +11,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = NULL, *current_node = *head;
 	unsigned int i = 0;
 
+	if (!head)
+		return (NULL);
 
 	while (i <= idx && current_node)
 	{

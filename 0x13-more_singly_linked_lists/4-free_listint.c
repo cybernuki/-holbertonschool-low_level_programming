9675074eb8de98723b2aaa209c571da5505a4e12
jhonatan arenas <1164@holbertonschool.com>
2019-11-07 17:46:38 +0000

    merge

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_listint - free a list
+ * @head: is the head of the list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}

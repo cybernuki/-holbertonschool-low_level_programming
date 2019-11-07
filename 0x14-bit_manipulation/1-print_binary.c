401517dcd4c3affb97e44fba83f2ac37bb4e0ece
jhonatan arenas <1164@holbertonschool.com>
2019-11-07 20:09:50 +0000

    Challenge 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <limits.h>
+#include "holberton.h"
+/**
+ * print_binary - prints the binary representation of a
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+	int firstBit = 0;
+
+	if (n == 0)
+		printf("0");
+
+	else
+	{
+		mask <<= 63;
+		while (mask)
+		{
+			if ((n & mask) == 0 && firstBit)
+				printf("0");
+			if ((n & mask) != 0)
+			{
+				printf("1");
+				firstBit = 1;
+			}
+			mask >>= 1;
+		}
+	}
+}

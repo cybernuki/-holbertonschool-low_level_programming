2e713535adb1cc44e8ea8b15166ca95977324872
jhonatan arenas <1164@holbertonschool.com>
2019-11-07 20:23:02 +0000

    Betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,3 +1,9 @@
+/**
+ * get_bit - gets the value of a bit in a index
+ * @n: is the number
+ * @index: is the index
+ * Return: the bit
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 64)

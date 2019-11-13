a9944a973acac6a00bb26a451bc29e7e5bad89bb
Cybernuki <1164@holbertonschool.com>
2019-11-12 22:21:46 -0500

    Challenge 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		return (-1);
 
-	hasW = write(f_d, text_content, sizeof(text_content) == -1);
+	hasW = write(f_d, text_content, sizeof(text_content));
 
 	if (hasW == -1)
 		return (-1);

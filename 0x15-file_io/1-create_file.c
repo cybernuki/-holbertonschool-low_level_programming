65bd6c832a76c78af4a3dc1ec4bafc1ac95970a3
Cybernuki <1164@holbertonschool.com>
2019-11-12 22:31:28 -0500

    Almost

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int f_d, hasW, size = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	f_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
@@ -17,8 +17,8 @@ int create_file(const char *filename, char *text_content)
 	if (f_d == -1)
 		return (-1);
 
-	if (!text_content)
-		return (-1);
+	if (text_content == NULL)
+		return (1);
 
 	while (text_content[size] != '\0')
 		size++;

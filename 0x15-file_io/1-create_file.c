0645b68b0661c1cfaa672ae5714eb4ead46b3c32
Cybernuki <1164@holbertonschool.com>
2019-11-12 22:13:37 -0500

    Challenge 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * create_file - creates a new fil
+ * @filename: name of the new file
+ * @text_content: is the text content to add
+ * Return: 1 if it was possible, -1 in otherwise;
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f_d;
+
+	if (!filename)
+		return (-1);
+
+	f_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (f_d == -1)
+		return (-1);
+
+	if (text_content)
+		if (write(f_d, text_content, sizeof(text_content) == -1))
+		    return (-1);
+
+	close(f_d);
+	return (1);
+}

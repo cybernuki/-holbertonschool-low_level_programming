51904279b5a2a5e5b38e504d8060e212d2cb2c05
Cybernuki <1164@holbertonschool.com>
2019-12-15 21:30:41 -0500

    add challenge 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - removes a node in a given position
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *to_delete = *head;
+
+	if (!(*head))
+		return (-1);
+	if (index == 0)
+	{
+		*head = to_delete->next;
+		free(to_delete);
+		return (1);
+	}
+
+	while (to_delete && index-- > 0)
+		to_delete = to_delete->next;
+
+	if (!to_delete)
+		return (-1);
+	else
+	{
+		to_delete->prev->next = to_delete->next;
+		to_delete->next->prev = to_delete->prev;
+		free(to_delete);
+		return (1);
+	}
+}

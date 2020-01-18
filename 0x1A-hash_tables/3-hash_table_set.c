1bf3a76459b71efe515710998dc0770c3225f608
Cybernuki <1164@holbertonschool.com>
2020-01-18 18:40:15 -0500

    Collision problem

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,25 +9,35 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *new_node = NULL, *node = NULL;
-    unsigned long int index;
+	hash_node_t *new_node = NULL, *node = NULL;
+	unsigned long int index;
 
-    if (!ht)
-        return (0);
+	if (!ht || !key || !value)
+		return (0);
 
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node)
-        return (0);
-    new_node->key = (char *) key;
-    new_node->value = (char *) value;
+	index = key_index((unsigned char *) key, ht->size);
+	node =  ht->array[index];
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			if (!node->value)
+				return (0);
+			return (1);
+		}
+		node = node->next;
+	}
 
-    index = key_index(key, ht->size);
-    node =  ht->array[index];
-    while(node)
-        node = node->next;
-    if (!node)
-        node = new_node;
-    else
-        node->next = new_node;
-    return (1);
+	new_node = malloc(sizeof(new_node));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (!new_node->key || !new_node->value)
+		return (0);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }

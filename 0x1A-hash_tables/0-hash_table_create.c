0d95996356cc652ca921453a93189ef118fc7dad
Cybernuki <1164@holbertonschool.com>
2020-01-18 15:54:15 -0500

    initi commit and challenge 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -
+ *
+ * Return: 
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table = NULL;
+    
+    table = malloc(sizeof(hash_table_t));
+    if (!table)
+    {
+        return NULL;
+    }
+
+    table->array = malloc(sizeof(table->array) * size);
+    if (!table->array)
+    {
+        free(table);
+        return NULL;
+    }
+
+    table->size = size;
+    return (table);
+}

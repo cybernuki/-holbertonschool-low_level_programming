2bb4add873aaa1870cc3080e2f1703cc045abae6
cybernuki <1164@holbertonschool.com>
2020-04-16 06:51:50 +0000

    implement iteration way

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,37 +1,6 @@
 #include "search_algos.h"
 
 
-
-/**
- *  aux_function - Let do the recursion for the binary search
- *  @array: Is the sub array
- *  @top: is the top index of the sub array
- *  @low: is the low index of the sub array
- *  @value: is the value to be searched
- *  Return: the index of the searched valued or -1
- */
-int aux_function(int *array, size_t top, size_t low, int value)
-{
-	size_t mid, i;
-
-	/*Print sub array */
-	printf("Searching in array: ");
-	for (i = low; i <= top; i++)
-		(i < top) ? printf("%d,", array[i]) : printf("%d\n", array[i]);
-
-	if (array[top] == value)
-		return (top);
-	if (array[low] == value)
-		return (low);
-
-	mid = (top + low) / 2;
-	if (top == low)
-		return (-1);
-	if (value > array[mid])
-		return (aux_function(array, top, mid + 1, value));
-	return (aux_function(array, mid - 1, low, value));
-}
-
 /**
  * binary_search - Do a binary search
  * @array: is the given array
@@ -41,7 +10,26 @@ int aux_function(int *array, size_t top, size_t low, int value)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (!array)
+	size_t i, left, right;
+
+	if (array == NULL)
 		return (-1);
-	return (aux_function(array, size - 1, (size_t) 0, value));
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-int linear_search_aux(int *, size_t, size_t, size_t, int);
 
 #endif
